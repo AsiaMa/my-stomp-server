d0864212751924dbf302e4b111aec6b96bfaa876
yazhouma <yazhouma3@creditease.cn>
2025-02-24 14:09:42 +0800

    init

---
+++ src/main/java/org/asia/mystompserver/MyStompServerApplication.java
@@ -0,0 +1,13 @@
+package org.asia.mystompserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStompServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStompServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/asia/mystompserver/MyStompServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asia.mystompserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStompServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
